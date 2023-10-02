5627095f003a26be91ab90925d6dcf732c801943
andytechh <136843666+andytechh@users.noreply.github.com>
2023-10-02 11:14:45 +0800

    Add files via upload

---
+++ aha.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+
+using namespace std;
+class task{
+	
+	public:
+		void task1(){
+	
+		
+		
+	}
+	
+};
+
+int main(){
+/*double height, width, area, perimeter;
+	
+	cout << "Enter value for height: ";
+	cin >> height;
+	cout << "Enter value for width: ";
+	cin >> width;
+	cout <<"\n";
+	area = height* width;
+	perimeter = 2*(height + width);
+	cout << fixed;
+	cout << "The area is: "<< setprecision(2)<< area<< endl;
+	cout << "The perimeter is : "<< setprecision(2)<< perimeter<< endl;
+	
+//Task 2
+		float fahrenheit, degreesC;
+		
+		cout << "Enter the value for fahrenheit: ";
+		cin >> fahrenheit;
+		degreesC = 5*(fahrenheit-32)/9;
+		cout << fixed;
+		cout << "\n";
+		cout << setprecision(1) << fahrenheit << "0 degrees Fahrenheit is equal to "<< setprecision(2) << degreesC << " degrees Celcius";
+//Task 3		
+	float length, width, height, volumeBox;
+	
+	cout << "Enter the value for a lenght: ";
+	cin >> length;
+	cout << "Enter the value for a width: ";
+	cin >> width;
+	cout << "Enter the value for a height: ";
+	cin >> height;
+	cout << "\n";
+	cout << fixed;
+	
+	volumeBox = length*width*height;
+	cout << "The volume of the box is: "<< setprecision(2) << volumeBox;
+	
+//Task 4
+	int a, b, x;
+	
+	cout << "Enter a value for variable a : ";
+	cin >> a;
+	cout << "Enter a value for variable b : ";
+	cin >> b;
+	x = -b/a;
+	
+	cout << "The value of variable x is " << x;
+	
+//Task 5
+	float hours_work, income, deduction, netincome;
+	
+	cout << "Enter the Hours of work: "; 
+	cin >> hours_work;
+	income = hours_work*750.00; 
+	deduction = income*(10.0/100.0); 
+	netincome = income - static_cast<int>(deduction);
+	cout << fixed;
+	cout << "Income: " << setprecision(2)<< income << endl; 
+	cout << "Deduction: " << setprecision(2) << deduction << endl; 
+	cout << "Net Income: " << setprecision(2) <<netincome << endl; 
+//task6 
+	float pants, shirts, totalAmount;
+	cout << "Enter the number of pants: "; 
+	cin >> pants; 
+	cout << "Enter the number of shirts: "; 
+	cin >> shirts;
+	cout << fixed;
+	cout << "\n";
+	totalAmount = (pants*700.00) + (shirts*315.00);
+	cout << "Total amount to pay: " << setprecision(2) <<totalAmount << endl;
+
+// task7 
+	float inches, foot;
+	cout << "Enter number of inches: "; 
+	cin >> inches; 
+	foot = inches / 12;
+	
+	cout << "\n";
+	cout << inches << " is equivalent to: " << fixed << setprecision(2) << foot;*/
+
+// task8 /*
+	float base, height, AreaofTriangle;
+	cout << "Enter the base value: "; 
+	cin >> base; 
+	cout << "Enter the height: "; 
+	cin >> height;
+	cout << fixed;
+	AreaofTriangle = (base * height)/2; 
+	cout << "\n";
+	cout << "The area of a triangle: " << setprecision(2) << AreaofTriangle << endl;
+
+
+	return 0;
+}
